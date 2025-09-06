c726dcdb3805c0335d0038abd57d5c54f053b71c
爱学啊开发 <ixueadev@163.com>
2025-09-06 11:57:16 +0800

    1001 创建项目和ViewModel

--- app/src/main/java/com/ixuea/course/MainActivity.kt
+++ app/src/main/java/com/ixuea/course/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.ixuea.course.ui.theme.MyWeatherTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels {
+        WeatherViewModelFactory()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/com/ixuea/course/WeatherViewModel.kt
@@ -0,0 +1,18 @@
+package com.ixuea.course
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class WeatherViewModel : ViewModel() {
+
+}
+
+class WeatherViewModelFactory() : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
+            return WeatherViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
